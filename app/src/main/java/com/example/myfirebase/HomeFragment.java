899c88b56c5c80a5251bf6f9fbe5dd1df5863fb9
dkhoang141298 <dkhoang141298@gmail.com>
2020-11-05 23:09:33 +0700

    RecyleView

--- app/src/main/java/com/example/myfirebase/HomeFragment.java
+++ app/src/main/java/com/example/myfirebase/HomeFragment.java
@@ -5,16 +5,64 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.myfirebase.model.Book;
+import com.example.myfirebase.recycleview.BookAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
+
+    private RecyclerView rvBooks;
+    private BookAdapter bookAdapter;
+    List<Book> mdata;
+    View v;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        v = inflater.inflate(R.layout.fragment_home, container, false);
+        initViews();
+        setupBookAdapter();
+        return v;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+        initmdataBooks();
+
+
+
+    }
+
+    private void setupBookAdapter() {
+        bookAdapter = new BookAdapter(mdata);
+        rvBooks.setAdapter(bookAdapter);
+        rvBooks.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    private void initmdataBooks() {
+        mdata = new ArrayList<>();
+        mdata.add(new Book(R.drawable.book1));
+        mdata.add(new Book(R.drawable.gatsby));
+        mdata.add(new Book(R.drawable.gatsby2));
+        mdata.add(new Book(R.drawable.nondesigner));
+        mdata.add(new Book(R.drawable.thefault));
+        mdata.add(new Book(R.drawable.themessy));
+    }
+
+    private void initViews() {
+        rvBooks = v.findViewById(R.id.rv_book);
     }
 }
---
+++ app/src/main/java/com/example/myfirebase/recycleview/BookAdapter.java
@@ -0,0 +1,73 @@
+package com.example.myfirebase.recycleview;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.example.myfirebase.R;
+import com.example.myfirebase.model.Book;
+
+import java.util.List;
+
+public class BookAdapter extends RecyclerView.Adapter<BookAdapter.bookviewholder> {
+
+    List<Book> mdata;
+
+
+    public BookAdapter(List<Book> mdata) {
+        this.mdata = mdata;
+    }
+
+    @NonNull
+    @Override
+    public bookviewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.item_manga,parent,false);
+        return new bookviewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull bookviewholder holder, int position) {
+
+        Glide.with(holder.itemView.getContext())
+                .load(mdata.get(position).getDrawableResource())
+                .transforms(new CenterCrop(), new RoundedCorners(16))
+                .into(holder.imgBook);
+        holder.ratingBar.setRating((float) 4.5);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mdata.size();
+    }
+
+    public class bookviewholder extends RecyclerView.ViewHolder{
+
+        ImageView imgBook,imgFav;
+        TextView title,description,author,pages,rate;
+        RatingBar ratingBar;
+
+        public bookviewholder(@NonNull View itemView) {
+            super(itemView);
+
+            imgBook = itemView.findViewById(R.id.item_book_img);
+            title = itemView.findViewById(R.id.item_book_title);
+            author = itemView.findViewById(R.id.item_book_author);
+            pages = itemView.findViewById(R.id.item_book_pagesrev);
+            rate = itemView.findViewById(R.id.item_book_score);
+            ratingBar = itemView.findViewById(R.id.item_book_ratingbar);
+
+
+        }
+    }
+}
+
